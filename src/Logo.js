d62ea25fb5eeb93c879ee5883264b2cc0ac9da12
kenilshahh <kenilshah1505@gmail.com>
2023-04-16 05:37:55 +0530

    pages added

--- src/Logo.js
+++ src/Logo.js
@@ -16,7 +16,7 @@ function Logo() {
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <a
-            href="#"
+            href="/createClub"
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Create A Club Now!
           </a>
